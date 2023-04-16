f7b9bc2521b1768590f758d415b001dc5ef8f481
LeftStyle <leftstylecn@gmail.com>
2023-04-16 19:14:07 +0800

    feat:loading icon

--- client/src/views/dashboard/autotest/index.vue
+++ client/src/views/dashboard/autotest/index.vue
@@ -9,9 +9,16 @@
                 <Button class="mr-3" type="default" @click="startTest(['dad', 'pc'])">DAD桌面版下单流程</Button>
             </Row>
 
-            <div class=" p-2 bg-blue-100 text-md" v-if="isLoading">
+            <div class=" p-2 flex bg-blue-100 text-md" v-if="isLoading">
                 正在测试<span class="inline-block mx-1 px-2 bg-green-400 text-white">{{ curSite.site }}</span>站
                 <span class="inline-block mx-1 px-2 bg-red-400 text-white">{{ curSite.platform }}</span>端
+                <svg class="animate-spin ml-1 mr-3 h-5 w-5 text-blue-600" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 24 24">
+                    <circle class=" opacity-40" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                    <path class=" opacity-90" fill="currentColor"
+                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
+                    </path>
+                </svg>
             </div>
 
             <Row class="my-3" v-if="!isLoading">
