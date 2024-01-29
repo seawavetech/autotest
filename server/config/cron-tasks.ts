2b446046c6b0e4cb5a05b78886dbe7690d2b5608
LeftStyle <leftstylecn@gmail.com>
2024-01-29 21:46:29 +0800

    feat: update sn

--- server/config/cron-tasks.ts
+++ server/config/cron-tasks.ts
@@ -197,7 +197,7 @@ export default (env:any)=>{
                 new Crawler({env,logCallback:prodCheckLogCallback}).start();
             },
             options: {
-                rule:'0 40 22 * * *',
+                rule:'0 40 2 * * *',
                 tz:'Asia/Shanghai'
             }
         }
--- server/src/module/prodRemoveCheck/txt2arr.ts
+++ server/src/module/prodRemoveCheck/txt2arr.ts
@@ -2,23 +2,33 @@
 import * as fs from 'fs-extra';
 import { shuffleArray } from '../../../utils/index'
 
-import sns from './sn.json';
+// import sns from './sn.json';
 
 
-// let data = fs.readFileSync('./sn.txt','utf-8');
-// let sns1 = shuffleArray(data.split('\n')); /* 乱序后分组 */
-// let sns = [];
-// let pageNumber = 48;
-// sns1.forEach((i,index)=>{
-//     let n = Math.floor(index/pageNumber)
-//     if(!sns[n]) sns[n] = []
-//     sns[n].push(i.split(','));
-// })
+let denySn=["715519","714856","715678","715679","715518","715512","714871","716132","714858",
+"716110","714860","715677","716216","715535","715676","716063","715510","715541","715536",
+"715681","715531","715513","715506","715507","716133","715537","715523","715683","716064",
+"714891","716217","715461","715673","714859","715539","715731","333696","714866","715674",
+"715285","715036","715521","715538","714865","715509","715534","715533","715508","715682",
+"715282","333922","715540","715532","715542","715680","715520","715675","715672","714905",
+"715335"]
+let data = fs.readFileSync('./data/sn.txt','utf-8');
+let sns1 = shuffleArray(data.split('\n').filter(i=>{
+    return !denySn.some(j=>j== String(i.split(',')[0]))
+})); /* 乱序后分组 */
+let sns = [];
+let pageNumber = 48;
 
-// fs.writeFileSync('../sn.json',JSON.stringify(sns))
+sns1.forEach((i,index)=>{
+    let n = Math.floor(index/pageNumber)
+    if(!sns[n]) sns[n] = []
+    sns[n].push(i.split(','));
+})
+
+fs.writeFileSync('./sn.json',JSON.stringify(sns))
 
 // console.log(sns)
-console.log(sns[0])
+// console.log(sns[0])
 
 
 
